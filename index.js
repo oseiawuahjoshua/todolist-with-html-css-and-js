ccdc019f3c01444041ceb8f0143380c0974a0bde
osei awuah joshua <oseiawuahjoshua2001@gmail.com>
2023-11-30 19:51:33 +0600

    saved all listItems update to the local storage

--- index.js
+++ index.js
@@ -1,49 +1,69 @@
-const addTask = document.querySelector('.add-task')
-const todolistContainer = document.querySelector('#todo-list')
-const inputTask = document.querySelector('.mytask')
-const allListItems= todolistContainer.children;
-const numberOfTasks= document.querySelector('.number-of-task')
+const addTask = document.querySelector('.add-task');
+const todolistContainer = document.querySelector('#todo-list');
+const inputTask = document.querySelector('.mytask');
+const numberOfTasks = document.querySelector('.number-of-task');
+const allchildren=todolistContainer.children;
 
+numberOfTasks.textContent = `${allchildren.length} Task(s)`;
 
-numberOfTasks.textContent='';
-
-function AttachRemoveButton(newList){
-    const removeBtn= document.createElement('BUTTON');
-    removeBtn.textContent= 'remove'
-    removeBtn.className='remove-btn'
-    newList.append(removeBtn)
-    
-    
-
-}
-for(let i=0; i< allListItems.length; i++){
-    AttachRemoveButton(allListItems[i]);
+function AttachRemoveButton(newList) {
+    const removeBtn = document.createElement('BUTTON');
+    removeBtn.textContent = 'remove';
+    removeBtn.className = 'remove-btn';
+    newList.append(removeBtn);
 }
 
-addTask.addEventListener('click', ()=>{
-    const newList= document.createElement('LI')
-    newList.className='newtask'
-    newList.textContent= inputTask.value;
+function saveToLocalStorage() {
+    const tasks = Array.from(todolistContainer.children).map(item => item.textContent);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+
+function loadFromLocalStorage() {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    for (const task of storedTasks) {
+        const newList = document.createElement('LI');
+        newList.className = 'newtask';
+        newList.textContent = task;
+        AttachRemoveButton(newList);
+        todolistContainer.append(newList);
+    }
+    numberOfTasks.textContent = `${storedTasks.length} Task(s)`;
+}
+
+addTask.addEventListener('click', () => {
+    const newList = document.createElement('LI');
+    newList.className = 'newtask';
+    newList.textContent = inputTask.value;
     AttachRemoveButton(newList);
     todolistContainer.append(newList);
-    numberOfTasks.textContent=`${allListItems.length}Task(s)`;
-    inputTask.value= '';
+    numberOfTasks.textContent = `${allchildren.length} Task(s)`;
+    saveToLocalStorage();
+    inputTask.value = '';
 
-})
+});
 
-
-todolistContainer.addEventListener('click', (event)=>{
-    if(event.target.tagName === 'BUTTON'){
+todolistContainer.addEventListener('click', (event) => {
+    if (event.target.tagName === 'BUTTON') {
         event.target.parentNode.remove();
+        saveToLocalStorage();
+        numberOfTasks.textContent = `${todolistContainer.children.length} Task(s)`;
     }
-})
-function displayDate(){
-    const dateToday= document.querySelector('.date')
-    let date= new Date();
-    let year = date.getFullYear()
-    let month= date.getMonth()
-    let day= date.getDay()
+});
 
-    dateToday.innerHTML= day + '-' + month + '-' + year ;
+
+function displayDate() {
+    const dateToday = document.querySelector('.date');
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getDay();
+    dateToday.innerHTML = day + '-' + month + '-' + year;
 }
-window.onload= displayDate();
+
+window.onload = () => {
+    displayDate();
+    loadFromLocalStorage();
+};
+
+
